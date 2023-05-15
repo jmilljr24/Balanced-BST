1f2799c73e330c56cfd6b5dd8eecc074606b0f2a
jmilljr24 <jmilljr24@gmail.com>
2023-05-15 09:29:28 -0400

    Add insert method

--- main.rb
+++ main.rb
@@ -27,6 +27,19 @@ class Tree
     root_node
   end
 
+  def insert(value, node = root)
+    return nil if value == node.data
+
+    # if value is < the root node move to left child
+    # if left child is nil, insert value else check new root node for greater or less than
+    if value < node.data
+      node.left.nil? ? node.left = Node.new(value) : insert(value, node.left)
+    else
+      node.right.nil? ? node.right = Node.new(value) : insert(value, node.right)
+    end
+  end
+
+  # Prints the node tree
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -37,4 +50,6 @@ end
 list = [1, 3, 4, 5, 2, 18, 12, 6, 4, 8, 7]
 
 bst = Tree.new(list)
+
+bst.insert(9)
 bst.pretty_print

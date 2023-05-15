66ed8eaa9db2471e8d0182cf924e74b8b720505a
jmilljr24 <jmilljr24@gmail.com>
2023-05-15 08:53:34 -0400

    Add Tree class with initial root node

--- main.rb
+++ main.rb
@@ -5,7 +5,24 @@ class Node
     @data = data
     @left_child = left_child
     @right_child = right_child
+  end
 end
 
 class Tree
+  attr_accessor :root, :data
+
+  def initialize(array)
+    @data = array.sort.uniq
+    @root = build_tree(data)
+  end
+
+  def build_tree(array)
+    mid = (array.size - 1) / 2
+    root_node = Node.new(array[mid])
+    p root_node
+  end
 end
+
+list = [1, 10, 4, 5, 6, 5, 8, 2, 12]
+
+bst = Tree.new(list)

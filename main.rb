9ff0d06d24bec075b421e1f3b3ce8148daaccdd3
jmilljr24 <jmilljr24@gmail.com>
2023-05-15 08:35:04 -0400

    Add node class

---
+++ main.rb
@@ -0,0 +1,11 @@
+class Node
+  attr_accessor :data, :left_child, :right_child
+
+  def initialize(data, left_child = nil, right_child = nil)
+    @data = data
+    @left_child = left_child
+    @right_child = right_child
+end
+
+class Tree
+end
